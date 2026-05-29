69d705980e32f054fd09544ef46b58b68b4df138
Ximena Hoyos <juandavidmh3@gmail.com>
2026-05-28 23:26:34 -0500

    Add useEffect to context provider for token handling

--- src/AppContext/FinanzasContext.jsx
+++ src/AppContext/FinanzasContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { setAccessToken } from "../services/authservice";
 //1. crear contexto
 const AppContext = createContext();
@@ -6,21 +6,22 @@ const AppContext = createContext();
 //2. Crear al provedor
 
 export const FinanzasContext = ({children})=>{
-    const [accessT, setAccessT] = useState(async()=>{
+    const [accessT, setAccessT] = useState(null);
+    useEffect(() => {
         const response = await axios.post(
-                    "https://finanzassimpleapi.onrender.com/api/financial/token/refresh/",
-                    {},
-                    {
-                        withCredentials: true,
-                    }
-                );
+            "https://finanzassimpleapi.onrender.com/api/financial/token/refresh/",
+            {},
+            {
+                withCredentials: true,
+            }
+        );
         if(response.data.access){
+            console.log(response)
             setAccessToken(response.data.access)
-            return response.data.access
-        }else{
-         return null   
+            setAccessT(response.data.access)
         }
-    });
+
+    }, [])
 
     const login = (access) => {
         setAccessToken(access);
