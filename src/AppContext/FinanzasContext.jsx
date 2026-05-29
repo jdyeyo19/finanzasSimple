66b22b76ba5525ebc8edd25a3303d59e3e2ae8d1
Juan David Martinez Hernandez <juandavidmh3@gmail.com>
2026-05-28 22:21:51 -0500

    Refactor FinanzasContext to use setAccessToken

--- src/AppContext/FinanzasContext.jsx
+++ src/AppContext/FinanzasContext.jsx
@@ -1,27 +1,24 @@
 import { createContext, useContext, useState } from "react";
-import { setTokens } from "../services/authservice";
+import { setAccessToken } from "../services/authservice";
 //1. crear contexto
 const AppContext = createContext();
 
 //2. Crear al provedor
 
 export const FinanzasContext = ({children})=>{
-    const [accessT, setAccessT] = useState(null)
-    const [refreshT, setRefreshT] = useState(null)
+    const [accessT, setAccessT] = useState("x");
 
-    const login = (access, refresh) => {
-        setTokens(access, refresh);
+    const login = (access) => {
+        setAccessToken(access);
         setAccessT(access);
-        setRefreshT(refresh);
     }
 
     const logout = () => {
         setAccessT(null);
-        setRefreshT(null);
     }
 
     return(
-        <AppContext.Provider value={{ accessT, refreshT, login, logout, setAccessT}}>
+        <AppContext.Provider value={{ accessT, login, logout, setAccessT}}>
             {children}
         </AppContext.Provider>
     )
